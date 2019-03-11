50fffa201ef63d4ae801ed25f5b213ed0fca0693
romain <r.robertpro@gmail.com>
2019-03-11 15:19:05 +0100

    first commit

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+include_once('inc/header.php');
+?>
+<h1>CONTACT</h1>
+
+<?php
+include_once('inc/footer.php');
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include_once('inc/header.php');
+?>
+<h1>ACCUEIL</h1>
+
+<?php
+include_once('inc/footer.php');
+?>
