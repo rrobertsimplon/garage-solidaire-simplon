a656f5157714ea738f3914d336f4a15f168c75f3
AdrienAubin <46442883+AdrienAubin@users.noreply.github.com>
2019-03-13 16:39:51 +0100

    CHANGE

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 include_once('inc/header.php');
 ?>
-<img class="Véhicules" src="images/Véhicules.jpg" alt="Véhicules">
 
+<section>
+    <div class="container-fluid">
+
+</div>
+<h1>Quelques informations sur les ventes..</h1>
+<p>Origine de nos véhicules</p>
+<ul>
+    <li>Tout d’abord, grâce aux relations étroites qu’elle entretient avec de nombreux garages Lillois,
+        elle achète régulièrement des véhicules à bas coût dans certains réseaux du territoire (Peugeot Citroën, Renault, …).</li>
+    <li>De la même façon, elle achète des véhicules auprès de collectivités locales avec lesquelles elle a conclu un partenariat.</li>
+    <li>Enfin, notre entreprise récupère de nombreux dons de particuliers. Ces dons encouragent nos actions et profitent à de nombreuses familles.</li>
+</ul>
+
+</section>
 <?php
 include_once('inc/footer.php');
 ?>
