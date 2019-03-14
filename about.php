5b86556ae484bf250167835ac719e61da6ba49b0
AdrienAubin <46442883+AdrienAubin@users.noreply.github.com>
2019-03-14 16:04:52 +0100

    change

--- about.php
+++ about.php
@@ -4,14 +4,20 @@ include_once('inc/header.php');
 <body>
 <body>
 <H1>A propos de nous</H1>
-    <video src="videos/videogarage.mp4"></video>
-    <h2> GSS vous accompagne sur vos petits et grands projets du quotidien grâce à son service web, mobile et mail de contenu.
-Bénéficiez gratuitement de l'aide et des conseils d'experts indépendants.</h2>
+<video controls>
+  <source src="vidéos/garage.mp4" type="video/mp4">
+  
+  <p>Votre navigateur ne prend pas en charge les vidéos HTML5.
+     Voici <a href="vidéos/garage.mp4">un lien pour télécharger la vidéo</a>.</p>
+</video>
+    <h3> GSS vous accompagne sur vos petits et grands projets du quotidien grâce à son service web, mobile et mail de contenu.
+Bénéficiez gratuitement de l'aide et des conseils d'experts indépendants.</h3>
 <br>
 <br>
-<h4>Sur le Web, on trouve les réponses à toutes les questions. Mais il faut passer des heures à démêler le farfelu, le génial, l'utile, et l'obsolète pour finir par trouver la bonne réponse.
-
-GSS vous apporte la solution sur un plateau : un site où de véritables experts apportent des réponses claires à toutes les questions de la vie quotidienne. </h4>
+<p>Sur le Web, on trouve les réponses à toutes les questions. Mais il faut passer des heures à démêler le farfelu, le génial, l'utile, 
+et l'obsolète pour finir par trouver la bonne réponse.
+GSS vous apporte la solution sur un plateau : un site où de véritables experts apportent des réponses claires à toutes les questions 
+de la vie quotidienne. </p>
 </body>
 
 <?php
--- index.php
+++ index.php
@@ -8,22 +8,24 @@ include_once('inc/header.php');
 </div>
 <h1>Quelques informations sur les ventes..</h1>
 <hr>
-<p>Origine de nos véhicules</p>
+<br><br>
+<h3>Origine de nos véhicules</h3>
 <ul>
     <li>Tout d’abord, grâce aux relations étroites qu’elle entretient avec de nombreux garages Lillois,
         elle achète régulièrement des véhicules à bas coût dans certains réseaux du territoire (Peugeot Citroën, Renault, …).</li>
     <li>De la même façon, elle achète des véhicules auprès de collectivités locales avec lesquelles elle a conclu un partenariat.</li>
     <li>Enfin, notre entreprise récupère de nombreux dons de particuliers. Ces dons encouragent nos actions et profitent à de nombreuses familles.</li>
 </ul>
+<br><br>
 
 <h3>Nos objectifs</h3>
 <ul>
 
 <li>Nos objectifs sont doubles : sécuriser tous les véhicules que nous mettons en vente tout en offrant des prix les plus bas possibles à nos clients.</li>
 </ul>
+<br><br>
 
-
-<h3>Pour qui ? Pour quoi?</h3>
+<h3>Pour qui ? Pourquoi?</h3>
 <ul>
 <li>L’accès à nos véhicules reste réservé à un public en difficulté et bénéficiaire des minimas sociaux – Le but est l’accès ou le maintien de la mobilité pour 
 favoriser les projets de vie et/ou professionnels : besoins du quotidien, vie sociale, emploi, formation,…</li>
