608a5a79c935ea5cc49963778403f05df470d280
AdrienAubin <46442883+AdrienAubin@users.noreply.github.com>
2019-03-15 13:38:13 +0100

    CHANGE DE OUF

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ include_once('inc/header.php');
 <li>L’accès à nos véhicules reste réservé à un public en difficulté et bénéficiaire des minimas sociaux – Le but est l’accès ou le maintien de la mobilité pour 
 favoriser les projets de vie et/ou professionnels : besoins du quotidien, vie sociale, emploi, formation,…</li>
 </ul>
+<h1>Ou nous trouver ?</h1>
+<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2536.313927738631!2d2.638411515554622!3d50.528331089631365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd2259b074e931%3A0xe7578f54a40e1108!2s35+Boulevard+Raymond+Poincar%C3%A9%2C+62400+B%C3%A9thune!5e0!3m2!1sfr!2sfr!4v1552653334832" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </section>
 <?php
 include_once('inc/footer.php');
